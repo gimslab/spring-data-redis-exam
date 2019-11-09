e158daa8f843b347d2a50ce18342c9cc3d1bf1c6
gimslab.com <gimslab.com@gmail.com>
2019-11-10 00:45:26 +0900

    set with optional

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,15 @@
 package com.example.demo;
 
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 @SpringBootApplication
 @Import(SpringRedisConfig.class)
@@ -15,24 +20,32 @@ public class DemoApplication {
 	}
 
 	private RedisTemplate<String, Object> redisTemplate;
+	private ApplicationArguments applicationArguments;
 
-	public DemoApplication(RedisTemplate<String, Object> redisTemplate) {
+	public DemoApplication(RedisTemplate<String, Object> redisTemplate, ApplicationArguments applicationArguments) {
 		this.redisTemplate = redisTemplate;
+		this.applicationArguments = applicationArguments;
 
 		System.out.println("+++++++++++++++++++++");
-		redisTest();
+		redisTest(actionArg());
+		actionArg();
 	}
 
-	private void redisTest() {
+	private String actionArg() {
+		List<String> args = applicationArguments.getNonOptionArgs();
+		return args.get(0);
+	}
 
+	private void redisTest(String action) {
+		System.out.println("action = "+action);
 		try {
 			ValueOperations<String, Object> values = redisTemplate.opsForValue();
 
-			values.set("myKey", "myVal");
+			if(action.equals("set"))
+				values.set("myKey", "myVal");
 
 			System.out.println("get result = " + values.get("myKey"));
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
